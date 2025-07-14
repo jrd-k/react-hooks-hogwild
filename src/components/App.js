ac3e80453bb2782dc709b8f2f1c6106bdc91ecc1
jared kiprop <jaredassociates@gmail.com>
2025-07-14 22:37:34 +0300

    adding app.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
+import HogTile from "./HogTile";
 
 function App() {
+	const[hoglist, setHoglist] = React.useState(hogs);
 	return (
 		<div className="App">
 			<Nav />
-		</div>
+		<div className="ui grid container">{
+				hoglist.map((hog) => {
+					return <HogTile key={hog.name} hog={hog} />;
+				})
+			}</div>
+			</div>
 	);
 }
 
